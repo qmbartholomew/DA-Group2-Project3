4edaf277d1ac1253b97e217702e1407bfc695d9a
Quentin Bartholomew <malcolmb98@yahoo.com>
2025-04-01 20:31:08 -0700

    angular branch

---
+++ Project-3/src/app/Project-3/src/app/chart-display/chart-display.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-display',
+  imports: [],
+  templateUrl: './chart-display.component.html',
+  styleUrl: './chart-display.component.css'
+})
+export class ChartDisplayComponent {
+
+}
