189f3fb28760b0e4d3e8f9e553c1db4e05b6407e
Quentin Bartholomew <malcolmb98@yahoo.com>
2025-04-01 21:24:40 -0700

    Angular updates

--- Project-3/src/app/Project-3/src/app/chart-display/chart-display.component.ts
+++ Project-3/src/app/Project-3/src/app/chart-display/chart-display.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chart-display',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './chart-display.component.html',
-  styleUrl: './chart-display.component.css'
+  styleUrls: ['./chart-display.component.css']
 })
 export class ChartDisplayComponent {
+  _selectedPair: string = '';
+  displayText: string = 'Please select a currency pair to view the chart.';
+  currencyPairs: string[] = ['EUR/USD', 'JPY/USD', 'CAD/USD'];
 
+  get selectedPair(): string {
+    return this._selectedPair;
+  }
+
+  set selectedPair(value: string) {
+    this._selectedPair = value;
+    this.displayText = value
+      ? `Chart for ${value} will be displayed here.`
+      : 'Please select a currency pair to view the chart.';
+  }
 }
