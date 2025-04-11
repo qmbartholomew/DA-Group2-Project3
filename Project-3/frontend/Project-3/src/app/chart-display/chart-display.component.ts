7631b822f87ef99449330664a6a7cde231349c71
Quentin Bartholomew <malcolmb98@yahoo.com>
2025-04-10 17:43:09 -0700

    cleanup

--- Project-3/frontend/Project-3/src/app/chart-display/chart-display.component.ts
+++ Project-3/frontend/Project-3/src/app/chart-display/chart-display.component.ts
@@ -1,18 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http'; 
+import { FormsModule } from '@angular/forms'; 
 import * as Plotly from 'plotly.js-dist-min';
 
 @Component({
   selector: 'app-chart-display',
   standalone: true,
-  imports: [CommonModule, FormsModule, HttpClientModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './chart-display.component.html',
   styleUrls: ['./chart-display.component.css']
 })
 export class ChartDisplayComponent {
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   _selectedPair: string = '';
   displayText: string = 'Please select a currency pair to view the chart.';
@@ -77,37 +76,8 @@ export class ChartDisplayComponent {
         let trades = profitsData.filter((p: number) => p !== 0);
         let profitable = trades.filter((p: number) => p > 0);
         let loss = trades.filter((p: number) => p < 0);
-        
-        // let avg = (arr: any[]) => arr.length ? arr.reduce((a, b) => a + b, 0) / arr.length : 0;
-        // let avgProfit = avg(profitable).toFixed(4);
-        // let avgLoss = avg(loss).toFixed(4);
 
-        let totalProfit = Number(trades.reduce((sum: number, val: number) => sum + val, 0).toFixed(4))
-        // will have to correct the currency with respect to the country(?)
-  
-        // Plotly.newPlot('avgProfitLossChart', [{
-        //   x: ['Average Profit', 'Average Loss'],
-        //   y: [avgProfit, avgLoss],
-        //   type: 'bar',
-        //   marker: { color: ['#6FCF97', '#EB5757'] }
-        // }], {
-        //   title: 'Average Profit vs. Average Loss per Trade',
-        //   xaxis: { title: { text: 'Trade Type' } },             // optional but clear
-        //   yaxis: { title: { text: 'Profit/Loss ($)' } }
-        // });
-
-        // Plotly.newPlot('avgProfitLossChart', [{
-        //   x: ['Total Profit/Loss'],
-        //   y: [(totalProfit)],
-        //   type: 'bar',
-        //   marker: { color: totalProfit >= 0 ? '#6FCF97' : '#EB5757' }, 
-        //   text: [`${totalProfit}`],
-        //   textposition: 'auto'
-        // }], {
-        //   title: 'Final Profit/Loss',
-        //   // xaxis: { title: { text: 'Trade Summary' } },
-        //   yaxis: { title: { text: 'Currency' }}
-        // });
+        let totalProfit = Number(trades.reduce((sum: number, val: number) => sum + val, 0).toFixed(4));
 
         Plotly.newPlot('avgProfitLossChart', [{
           x: ['Total Profit/Loss'],
@@ -127,21 +97,6 @@ export class ChartDisplayComponent {
           },
           yaxis: { title: { text: this.selectedPair } }
         });
-  
-        // Plotly.newPlot('winLossChart', [{
-        //   labels: ['Winning Trades', 'Losing Trades'],
-        //   values: [profitable.length, loss.length],
-        //   type: 'pie',
-        //   marker: { colors: ['#6FCF97', '#EB5757'] },
-        //   textinfo: 'percent',
-        //   textposition: 'inside',
-        //   insidetextorientation: 'radial'
-        // }], {
-        //   title: 'Win vs. Loss Distribution',
-        //   height: 400,
-        //   width: 400,
-        //   margin: { t: 50, b: 50, l: 50, r: 50 }
-        // });
 
         Plotly.newPlot('winLossChart', [{
           labels: ['Winning Trades', 'Losing Trades'],
@@ -157,12 +112,12 @@ export class ChartDisplayComponent {
             font: { 
               size: 24,
               family: 'Times' },
-            x: 0.35,               // centers the title
+            x: 0.35,
             xanchor: 'center'
           },
           height: 400,
           width: 700,
-          margin: { t: 60, b: 50, l: 0, r: 150 } // top margin gives room for the title
+          margin: { t: 60, b: 50, l: 0, r: 150 }
         });
       });
   }
